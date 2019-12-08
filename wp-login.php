517138cee5a29756709e09a3194650c0987dbffc
William O. Pate II <willpate@pm.me>
2019-12-08 15:06:20 -0600

    Initial commit of main html files from WordPress 8DEC2019

---
+++ wp-login.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; San Antonio Review &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script src='https://c0.wp.com/c/5.3/wp-includes/js/jquery/jquery.js'></script>
+<script src='https://c0.wp.com/c/5.3/wp-includes/js/jquery/jquery-migrate.min.js'></script>
+<script src='https://c0.wp.com/p/jetpack/8.0/modules/sso/jetpack-sso-login.js'></script>
+<link rel='stylesheet' id='dashicons-css'  href='https://c0.wp.com/c/5.3/wp-includes/css/dashicons.min.css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://c0.wp.com/c/5.3/wp-includes/css/buttons.min.css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://c0.wp.com/c/5.3/wp-admin/css/forms.min.css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://c0.wp.com/c/5.3/wp-admin/css/l10n.min.css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://c0.wp.com/c/5.3/wp-admin/css/login.min.css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='https://c0.wp.com/p/jetpack/8.0/_inc/genericons/genericons/genericons.css' media='all' />
+<link rel='stylesheet' id='jetpack-sso-login-css'  href='https://c0.wp.com/p/jetpack/8.0/modules/sso/jetpack-sso-login.css' media='all' />
+			<style>
+				.jetpack-sso .message {
+					margin-top: 20px;
+				}
+
+				.jetpack-sso #login .message:first-child,
+				.jetpack-sso #login h1 + .message {
+					margin-top: 0;
+				}
+			</style>
+			<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://i0.wp.com/sa-review.com/wp-content/uploads/2019/06/SA-Review-FB-Profile-PhotoArtboard-1.png?fit=32%2C32&#038;ssl=1" sizes="32x32" />
+<link rel="icon" href="https://i0.wp.com/sa-review.com/wp-content/uploads/2019/06/SA-Review-FB-Profile-PhotoArtboard-1.png?fit=192%2C192&#038;ssl=1" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://i0.wp.com/sa-review.com/wp-content/uploads/2019/06/SA-Review-FB-Profile-PhotoArtboard-1.png?fit=180%2C180&#038;ssl=1" />
+<meta name="msapplication-TileImage" content="https://i0.wp.com/sa-review.com/wp-content/uploads/2019/06/SA-Review-FB-Profile-PhotoArtboard-1.png?fit=270%2C270&#038;ssl=1" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us jetpack-sso jetpack-sso-form-display">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+					<div id="jetpack-sso-wrap">
+			
+
+			<div id="jetpack-sso-wrap__action">
+				<a rel="nofollow" href="/wp-login.php?action=jetpack-sso" class="jetpack-sso button button-primary"><span><span class="genericon genericon-wordpress"></span> Log in with WordPress.com</span></a>
+									<p>
+						You can now save time spent logging in by connecting your WordPress.com account to San Antonio Review.					</p>
+							</div>
+
+							<div class="jetpack-sso-or">
+					<span>Or</span>
+				</div>
+
+				<a href="/wp-login.php?jetpack-sso-show-default-form=1" class="jetpack-sso-toggle wpcom">
+					Log in with username and password				</a>
+
+				<a href="/wp-login.php?jetpack-sso-show-default-form=0" class="jetpack-sso-toggle default">
+					Log in with WordPress.com				</a>
+					</div>
+					<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+									<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+								</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="/">
+		&larr; Back to San Antonio Review		</a></p>
+			</div>
+	<link rel='stylesheet' id='jetpack_css-css'  href='/wp-content/plugins/jetpack/css/jetpack.css?ver=8.0' media='all' />
+<script>
+var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src='https://c0.wp.com/c/5.3/wp-includes/js/zxcvbn-async.min.js'></script>
+<script>
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+</script>
+<script src='https://c0.wp.com/c/5.3/wp-admin/js/password-strength-meter.min.js'></script>
+<script src='https://c0.wp.com/c/5.3/wp-includes/js/underscore.min.js'></script>
+<script>
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src='https://c0.wp.com/c/5.3/wp-includes/js/wp-util.min.js'></script>
+<script>
+var userProfileL10n = {"warn":"Your new password has not been saved.","warnWeak":"Confirm use of weak password","show":"Show","hide":"Hide","cancel":"Cancel","ariaShow":"Show password","ariaHide":"Hide password"};
+</script>
+<script src='https://c0.wp.com/c/5.3/wp-admin/js/user-profile.min.js'></script>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+		<div class="clear"></div>
+	</body>
+	</html>
+	
+<!--
+Performance optimized by W3 Total Cache. Learn more: https://www.w3-edge.com/products/
+
+Object Caching 15/18 objects using disk
+Page Caching using disk: enhanced (Requested URI is rejected) 
+Database Caching using disk
+
+Served from: sa-review.com @ 2019-12-08 14:31:06 by W3 Total Cache
+-->
